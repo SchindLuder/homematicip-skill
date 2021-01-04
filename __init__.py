4089fca8c29d9c03ac0239bdb841416f80b1f16c
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-01-04 22:17:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 class Homematicip(MycroftSkill):
 	def __init__(self):
 		self.log.info('__init__')
@@ -6,10 +6,9 @@ class Homematicip(MycroftSkill):
 	
 	def initialize(self):
 		self.log.info('initialized')
-		self.register_intent_file("homematicip.get.temperature.intent",
-                                      self.handle_get_temperature)
+		self.register_intent_file("homematicip.get.temperature.intent",self.handle_get_temperature)
 		
-	#@intent_file_handler('homematicip.get.temperature.intent')
+	@intent_handler('homematicip.get.temperature.intent')
 	def handle_get_temperature(self, message):
 		self.log.info('get temperature intent detected')
 		self.speak('Wait i will try to read the temperature')
