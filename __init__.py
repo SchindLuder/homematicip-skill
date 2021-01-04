11f0f178e257b6c4262538821f93825988e2b398
Matthias Schindler <matthiasschindler92@gmx.net>
2021-01-04 20:37:04 +0100

    initial commit

--- __init__.py
+++ __init__.py
@@ -5,10 +5,19 @@ class Homematicip(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('homematicip.intent')
-    def handle_homematicip(self, message):
-        self.speak_dialog('homematicip')
+    @intent_file_handler('homematicip.get.temperature.intent')
+    def handle_get_temperature(self, message):
+        self.speak('Wait i will try to read the temperature')
+        room_type = message.data.get('room')
 
+        if room_type is None:
+           self.speak('This room type is unknown to me')
+
+	self.speak('I got the room. Here we go!')
+
+    def converse(self, utterances,lang):
+        self.log.info("converse of homematic skill called")
+        return False
 
 def create_skill():
     return Homematicip()
