656e9a4b8dfaecffd1ba76e260ba7f6af0869729
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-01-04 21:26:53 +0100

    Update __init__.py
    fixed tabs only in file

--- __init__.py
+++ __init__.py
@@ -1,21 +1,19 @@
 from mycroft import MycroftSkill, intent_file_handler
-
-
 class Homematicip(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+	def __init__(self):
+        	MycroftSkill.__init__(self)
 
-    @intent_file_handler('homematicip.get.temperature.intent')
-    def handle_get_temperature(self, message):
-	self.log.info('get temperature intent detected')
-	self.speak('Wait i will try to read the temperature')
-	room_type = message.data.get('room')
-
-	if room_type is None:
-		self.speak('This room type is unknown to me')
-	elif
-		self.speak('I got the room. Here we go!')
-	
+    	@intent_file_handler('homematicip.get.temperature.intent')
+    	def handle_get_temperature(self, message):
+		self.log.info('get temperature intent detected')
+		self.speak('Wait i will try to read the temperature')
+		room_type = message.data.get('room')
+		
+		if room_type is None:
+			self.speak('This room type is unknown to me')
+		elif
+			self.speak('I got the room. Here we go!')
+			
 def create_skill():
-    return Homematicip()
+	return Homematicip()
 
