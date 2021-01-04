bc7d13a1cde5358bb8a7f3a64a7b4620efa00d93
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-01-04 21:34:07 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 from mycroft import MycroftSkill, intent_file_handler
 class Homematicip(MycroftSkill):
 	def __init__(self):
-        	MycroftSkill.__init__(self)
-
-    	@intent_file_handler('homematicip.get.temperature.intent')
-    	def handle_get_temperature(self, message):
+		MycroftSkill.__init__(self)
+		
+	@intent_file_handler('homematicip.get.temperature.intent')
+	def handle_get_temperature(self, message):
 		self.log.info('get temperature intent detected')
 		self.speak('Wait i will try to read the temperature')
 		room_type = message.data.get('room')
-		
+
 		if room_type is None:
 			self.speak('This room type is unknown to me')
 		elif
