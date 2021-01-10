f1a1eb19c1cc477cfbc854692415da08f5cc3000
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-01-10 18:57:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,8 +3,7 @@ import re
 import time
 import subprocess
 import time
-from alexa_led_pattern import AlexaLedPattern
-from google_home_led_pattern import GoogleHomeLedPattern
+import numpy
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 class Homematicip(MycroftSkill):
@@ -14,7 +13,7 @@ class Homematicip(MycroftSkill):
 	def initialize(self):
 		self.clientPath = self.settings.get('HmipClientPath')
 		self.pixels = Pixels()
-		self.pixels.pattern = GoogleHomeLedPattern(show=pixels.show)
+		self.pixels.pattern = AlexaLedPattern(show=pixels.show)
 					
 	@intent_handler('homematicip.get.temperature.intent')
 	def handle_get_temperature(self, message):		
@@ -131,6 +130,59 @@ class Pixels:
 
         self.dev.show()
 
+class AlexaLedPattern(object):
+    def __init__(self, show=None, number=12):
+        self.pixels_number = number
+        self.pixels = [0] * 4 * number
+
+        if not show or not callable(show):
+            def dummy(data):
+                pass
+            show = dummy
+
+        self.show = show
+        self.stop = False
+
+    def wakeup(self, direction=0):
+        position = int((direction + 15) / (360 / self.pixels_number)) % self.pixels_number
+
+        pixels = [0, 0, 0, 24] * self.pixels_number
+        pixels[position * 4 + 2] = 48
+
+        self.show(pixels)
+
+    def listen(self):
+        pixels = [0, 0, 0, 24] * self.pixels_number
+
+        self.show(pixels)
+
+    def think(self):
+        pixels  = [0, 0, 12, 12, 0, 0, 0, 24] * self.pixels_number
+
+        while not self.stop:
+            self.show(pixels)
+            time.sleep(0.2)
+            pixels = pixels[-4:] + pixels[:-4]
+
+    def speak(self):
+        step = 1
+        position = 12
+        while not self.stop:
+            pixels  = [0, 0, position, 24 - position] * self.pixels_number
+            self.show(pixels)
+            time.sleep(0.01)
+            if position <= 0:
+                step = 1
+                time.sleep(0.4)
+            elif position >= 12:
+                step = -1
+                time.sleep(0.4)
+
+            position += step
+
+    def off(self):
+        self.show([0] * 4 * 12)
+	
 				
 def create_skill():
 	return Homematicip()
