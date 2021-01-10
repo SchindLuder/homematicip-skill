f0252eedfc356cff92306760982a895e2aed5540
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-01-10 19:09:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ class Homematicip(MycroftSkill):
 	def initialize(self):
 		self.clientPath = self.settings.get('HmipClientPath')
 		self.pixels = Pixels()		
-		self.pixels.pattern =  = AlexaLedPattern(show = self.pixels.show)
+		self.pixels.pattern =  AlexaLedPattern(show = self.pixels.show)
 		
 					
 	@intent_handler('homematicip.get.temperature.intent')
