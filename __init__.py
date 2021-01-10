3bb6ee0e9661db583b4faf1f869f7e6ff0aef201
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-01-10 19:12:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,9 +12,7 @@ class Homematicip(MycroftSkill):
 		
 	def initialize(self):
 		self.clientPath = self.settings.get('HmipClientPath')
-		self.pixels = Pixels()		
-		self.pixels.pattern =  AlexaLedPattern(show = self.pixels.show)
-		
+		self.pixels = Pixels()	
 					
 	@intent_handler('homematicip.get.temperature.intent')
 	def handle_get_temperature(self, message):		
@@ -73,64 +71,7 @@ class Homematicip(MycroftSkill):
 				self.pixels.speak()
 				self.speak_dialog('say.temperature', {'room': room_type, 'temperature': temperature})
 				self.pixels.off()
-class Pixels:
-    PIXELS_N = 12
-
-    def __init__(self, pattern):
-        self.pattern = pattern(show=self.show)
-
-        self.dev = apa102.APA102(num_led=self.PIXELS_N)
-        
-        self.power = LED(5)
-        self.power.on()
-
-        self.queue = Queue.Queue()
-        self.thread = threading.Thread(target=self._run)
-        self.thread.daemon = True
-        self.thread.start()
-
-        self.last_direction = None
-
-    def wakeup(self, direction=0):
-        self.last_direction = direction
-        def f():
-            self.pattern.wakeup(direction)
-
-        self.put(f)
-
-    def listen(self):
-        if self.last_direction:
-            def f():
-                self.pattern.wakeup(self.last_direction)
-            self.put(f)
-        else:
-            self.put(self.pattern.listen)
-
-    def think(self):
-        self.put(self.pattern.think)
-
-    def speak(self):
-        self.put(self.pattern.speak)
-
-    def off(self):
-        self.put(self.pattern.off)
-
-    def put(self, func):
-        self.pattern.stop = True
-        self.queue.put(func)
-
-    def _run(self):
-        while True:
-            func = self.queue.get()
-            self.pattern.stop = False
-            func()
-
-    def show(self, data):
-        for i in range(self.PIXELS_N):
-            self.dev.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
-
-        self.dev.show()
-
+				
 class AlexaLedPattern(object):
     def __init__(self, show=None, number=12):
         self.pixels_number = number
@@ -184,6 +125,63 @@ class AlexaLedPattern(object):
     def off(self):
         self.show([0] * 4 * 12)
 	
+class Pixels:
+    PIXELS_N = 12
+
+    def __init__(self, pattern=AlexaLedPattern):
+        self.pattern = pattern(show=self.show)
+
+        self.dev = apa102.APA102(num_led=self.PIXELS_N)
+        
+        self.power = LED(5)
+        self.power.on()
+
+        self.queue = Queue.Queue()
+        self.thread = threading.Thread(target=self._run)
+        self.thread.daemon = True
+        self.thread.start()
+
+        self.last_direction = None
+
+    def wakeup(self, direction=0):
+        self.last_direction = direction
+        def f():
+            self.pattern.wakeup(direction)
+
+        self.put(f)
+
+    def listen(self):
+        if self.last_direction:
+            def f():
+                self.pattern.wakeup(self.last_direction)
+            self.put(f)
+        else:
+            self.put(self.pattern.listen)
+
+    def think(self):
+        self.put(self.pattern.think)
+
+    def speak(self):
+        self.put(self.pattern.speak)
+
+    def off(self):
+        self.put(self.pattern.off)
+
+    def put(self, func):
+        self.pattern.stop = True
+        self.queue.put(func)
+
+    def _run(self):
+        while True:
+            func = self.queue.get()
+            self.pattern.stop = False
+            func()
+
+    def show(self, data):
+        for i in range(self.PIXELS_N):
+            self.dev.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
+
+        self.dev.show()
 				
 def create_skill():
 	return Homematicip()
